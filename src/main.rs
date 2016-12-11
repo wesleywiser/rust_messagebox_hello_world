2af6b6697b7f3e3c5c4e39319ad24ba5384a11ea
Wesley Wiser <wwiser@gmail.com>
2016-12-10 22:09:40 -0500

    Use unicode

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 extern crate user32;
 extern crate winapi;
 
-use std::ffi::CString;
-use user32::MessageBoxA;
+use user32::MessageBoxW;
 use winapi::winuser::{MB_OK, MB_ICONINFORMATION};
 
 fn main() {
-    let lp_text = CString::new("Hello, world!").unwrap();
-    let lp_caption = CString::new("MessageBox Example").unwrap();
+    let lp_text : Vec<u16> = "Hello, world! \u{1F60E}\0".encode_utf16().collect();
+    let lp_caption : Vec<u16> = "MessageBox Example\0".encode_utf16().collect();
 
     unsafe {
-        MessageBoxA(
-            std::ptr::null_mut(), 
-            lp_text.as_ptr(), 
-            lp_caption.as_ptr(), 
+        MessageBoxW(
+            std::ptr::null_mut(),
+            lp_text.as_ptr(),
+            lp_caption.as_ptr(),
             MB_OK | MB_ICONINFORMATION
         );
     }
